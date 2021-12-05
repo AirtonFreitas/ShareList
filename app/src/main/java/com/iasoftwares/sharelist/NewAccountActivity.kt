2c29e45bb8aee4713a6f096e0bd39a4e2b03a076
AirtonFreitas <airtonsiqueiradefreitas@gmail.com>
2021-12-05 12:57:30 -0300

    activitys Account

---
+++ app/src/main/java/com/iasoftwares/sharelist/NewAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.iasoftwares.sharelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_account)
+    }
+}
