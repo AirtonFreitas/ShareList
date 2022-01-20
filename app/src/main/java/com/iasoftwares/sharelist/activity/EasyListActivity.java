03688af887edf132d2803a91a4b7690329073aaf
AirtonFreitas <airtonsiqueiradefreitas@gmail.com>
2022-01-20 00:09:23 -0300

    implementa tela de lista prontas

--- app/src/main/java/com/iasoftwares/sharelist/activity/EasyListActivity.java
+++ app/src/main/java/com/iasoftwares/sharelist/activity/EasyListActivity.java
@@ -2,15 +2,29 @@ package com.iasoftwares.sharelist.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.iasoftwares.sharelist.R;
 
 public class EasyListActivity extends AppCompatActivity {
+    private Button bntBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_list);
+        bntBack = findViewById(R.id.btnBackID);
+
+        bntBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EasyListActivity.this, MainActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            }
+        });
     }
 }
