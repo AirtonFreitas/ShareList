ca0d964516f99a93fb288d8a68e6a5b325c5cab5
AirtonFreitas <airtonsiqueiradefreitas@gmail.com>
2022-02-06 15:43:14 -0300

    ajusta handler deprecated

--- app/src/main/java/com/iasoftwares/sharelist/activity/SplashActivity.kt
+++ app/src/main/java/com/iasoftwares/sharelist/activity/SplashActivity.kt
@@ -4,16 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.iasoftwares.sharelist.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val mIntent = Intent(this, MainActivity::class.java)
             startActivity(mIntent)
             finish()
-        },3000)
+        }, 3000)
     }
 }
